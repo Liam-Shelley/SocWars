77aea29c739555e9870f0823d75670dbe7ca66b0
LiamShelley <66814870+SomeOtherCyclist@users.noreply.github.com>
2026-01-29 18:09:15 +1100

    Beds only disabled in survival

--- src/main/java/com/soc/mixin/DisableSleepingInBeds.java
+++ src/main/java/com/soc/mixin/DisableSleepingInBeds.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,6 +17,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class DisableSleepingInBeds {
 	@Inject(method = "onUse", at = @At(value = "HEAD"), cancellable = true)
 	void socwars_bedwarsShopOpen(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
-		cir.setReturnValue(ActionResult.FAIL);
+		if (player.getGameMode() == GameMode.SURVIVAL) cir.setReturnValue(ActionResult.FAIL);
 	}
 }
