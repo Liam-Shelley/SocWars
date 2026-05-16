79bc3c4434b4179b99e52568b7e2fb8c3b7c7ecd
LiamShelley <66814870+Liam-Shelley@users.noreply.github.com>
2026-05-17 09:15:22 +1000

    Actually fixed it this time

--- src/main/java/com/soc/blocks/KitBlock.java
+++ src/main/java/com/soc/blocks/KitBlock.java
@@ -2,12 +2,10 @@ package com.soc.blocks;
 
 import com.mojang.serialization.MapCodec;
 import com.soc.blocks.blockentities.KitBlockEntity;
-import com.soc.player.PlayerDataManager;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
@@ -33,13 +31,13 @@ public class KitBlock extends BlockWithEntity {
         if (player.isCreative()) {
             player.openHandledScreen(blockEntity);
         } else {
-            this.openKitSelectionScreen();
+            this.openKitSelectionScreen(blockEntity);
         }
 
         return ActionResult.SUCCESS;
     }
 
-    private void openKitSelectionScreen() {} //Mixin target for client only
+    private void openKitSelectionScreen(KitBlockEntity blockEntity) {} //Mixin target for client only
 
     @Override
     public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
